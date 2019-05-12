485fbe86671bda422717461558c6099b92564f45
jennylv90 <13926583293@163.com>
2019-05-12 14:42:34 +0800

    first commit

---
+++ Microsoft VS Code/resources/app/out/vs/code/electron-browser/processExplorer/processExplorerMain.nls.js
@@ -0,0 +1,5 @@
+/*!--------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+define("vs/code/electron-browser/processExplorer/processExplorerMain.nls",{"vs/code/electron-browser/processExplorer/processExplorerMain":["CPU %","Memory (MB)","pid","Name","Kill Process","Force Kill Process","Copy","Copy All","Copy All"]});
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/5944e81f3c46a3938a82c701f96d7a59b074cfdc/core/vs\code\electron-browser\processExplorer\processExplorerMain.nls.js.map
---
+++ Microsoft VS Code/resources/app/out/vs/workbench/services/files/node/watcher/unix/watcherApp.nls.js
@@ -0,0 +1,5 @@
+/*!--------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+define("vs/workbench/services/files/node/watcher/unix/watcherApp.nls",{"vs/base/node/processes":["Can't execute a shell command on a UNC drive."]});
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/5944e81f3c46a3938a82c701f96d7a59b074cfdc/core/vs\workbench\services\files\node\watcher\unix\watcherApp.nls.js.map
